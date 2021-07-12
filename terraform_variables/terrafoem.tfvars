74c4b5340b8b4043f7452254a4e913cd4d440997
chi <chinedu.onyeoka@gmail.com>
2021-07-12 19:19:25 +0100

    wip

---
+++ terraform_variables/terrafoem.tfvars
@@ -0,0 +1,2 @@
+resource_group_name ="bookRg"
+application_name ="book"
---
+++ terraform_variables/variables.tf
@@ -0,0 +1,10 @@
+variable "resource_group_name" {
+description ="Name of the resource group"
+}
+variable "location" {
+description ="Location of the resource"
+default ="West Europe"
+}
+variable "application_name" {
+description ="Name of the application"
+}
